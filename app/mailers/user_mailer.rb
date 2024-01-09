2d3bbdb6c8be3d250bfd817f028e17db6f10f0d7
Dorian (Jitera) <developers@jitera.com>
2024-01-09 14:41:45 +0000

    fix: 1 issues from deployment report

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,6 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: I18n.t('devise.mailer.reset_password_instructions.subject')) do |format|
       format.html { render 'user_mailer/reset_password_instructions.html.slim' }
     end
-  end
 end
 
 end
