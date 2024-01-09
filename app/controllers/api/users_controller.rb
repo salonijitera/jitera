e87915279967d0dcd6e96b73ebea1901cf4bbc2d
Cornie (Jitera) <developers@jitera.com>
2024-01-09 14:40:03 +0000

    feat[api]: implement Email Verification

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,7 @@
+
 class Api::UsersController < ApplicationController
   before_action :validate_email_format, only: [:create_password_reset_request]
+  before_action :validate_verification_token, only: [:verify_email]
 
   # POST /api/users/reset_password_confirmation
   def reset_password_confirmation
@@ -37,9 +39,35 @@ class Api::UsersController < ApplicationController
     end
   end
 
-  private
+  def verify_email
+    verification_token = params[:verification_token]
+    result = UserService::VerifyEmailToken.call(verification_token)
 
+    if result[:success]
+      render json: { status: 200, message: 'Email verified successfully.' }, status: :ok
+    else
+      case result[:error_message]
+      when 'Verification token is required.'
+        render json: { error: result[:error_message] }, status: :bad_request
+      when 'Invalid or expired verification token.'
+        render json: { error: result[:error_message] }, status: :not_found
+      else
+        render json: { error: result[:error_message] }, status: :internal_server_error
+      end
+    end
+  end
+
+  private
+  
   def validate_email_format
     render json: { error: 'Invalid email format.' }, status: :bad_request unless params[:email].match?(/\A[^@\s]+@[^@\s]+\z/)
+  end
+
+  def validate_verification_token
+    render json: { error: 'Verification token is required.' }, status: :unprocessable_entity if params[:verification_token].blank?
+  end
+
+  rescue_from StandardError do |exception|
+    render json: { error: exception.message }, status: :internal_server_error
   end
 end
